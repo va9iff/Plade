4c730e256795b4be7e20165d3ffae19a7888e071
va9iff <va9iff@gmail.com>
2021-08-22 19:49:58 +0400

    tests to revert

--- import.js
+++ import.js
@@ -3,6 +3,12 @@ function loadScript(src) {
 	document.write(`<script src="${src}"></script>`);
 }
 
+<<<<<<< HEAD
 loadScript('https://raw.githubusercontent.com/va9iff/Plade/main/Velegant.js')
 loadScript('https://raw.githubusercontent.com/va9iff/Plade/main/Vome.js')
 loadScript('https://raw.githubusercontent.com/va9iff/Plade/main/Dynamic.js')
+=======
+loadScript('Velegant.js')
+loadScript('Vome.js')
+loadScript('Dynamic.js')
+>>>>>>> parent of 81b2fae (simple example with only one script tag import test)
